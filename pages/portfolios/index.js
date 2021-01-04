eb77e7a04b682693aa36ed4717f649112914c521
jocsanmairena <jocsan.mairena@gmail.com>
2021-01-04 17:57:52 -0600

    fetch portfolio data with axios and graphql: getInitialProps and onClick fetch

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,79 +1,100 @@
-const apiCall = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ testingData: 'Just some testing data' })
-    }, 200)
-  })
+import axios from 'axios'
+
+//making a axios request to retrive portfolios data using graphql
+const fetchPortfolios = () => {
+  const query = `
+    query Porfolios {
+        portfolios {
+            _id
+            title
+            companyWebsite
+            location
+            jobTitle
+            description
+        }
+    }`
+
+  return axios
+    .post('http://localhost:3000/graphql', { query })
+    .then(({ data: graph }) => {
+      //destructurize data as graph (data.data.portfolios to graph.data.portfolios )
+      return graph.data.portfolios
+    })
 }
 
-const Portfolios = props => (
-  <>
-    {props.testingData}
-    <section className='section-title'>
-      <div className='px-2'>
-        <div className='pt-5 pb-4'>
-          <h1>Portfolios</h1>
+const Portfolios = ({ portfolios }) => {
+  //destructurized portfolios from pageProps
+  return (
+    <>
+      <section className='section-title'>
+        <div className='px-2'>
+          <div className='pt-5 pb-4'>
+            <h1>Portfolios</h1>
+          </div>
         </div>
-      </div>
-    </section>
-    <section className='pb-5'>
-      <div className='row'>
-        <div className='col-md-4'>
-          <div className='card subtle-shadow no-border'>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
-              <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-              <p className='card-text fs-2'>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+        <button className='btn btn-primary' onClick={fetchPortfolios}>
+          Fetch Data
+        </button>
+      </section>
+      {JSON.stringify(portfolios)}
+      <section className='pb-5'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='card subtle-shadow no-border'>
+              <div className='card-body'>
+                <h5 className='card-title'>Card title</h5>
+                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
+                <p className='card-text fs-2'>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <div className='card-footer no-border'>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </div>
             </div>
-            <div className='card-footer no-border'>
-              <small className='text-muted'>Last updated 3 mins ago</small>
+          </div>
+          <div className='col-md-4'>
+            <div className='card subtle-shadow no-border'>
+              <div className='card-body'>
+                <h5 className='card-title'>Card title</h5>
+                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
+                <p className='card-text fs-2 '>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <div className='card-footer no-border'>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='card subtle-shadow no-border'>
+              <div className='card-body'>
+                <h5 className='card-title'>Card title</h5>
+                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
+                <p className='card-text fs-2 '>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+              <div className='card-footer no-border'>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </div>
             </div>
           </div>
         </div>
-        <div className='col-md-4'>
-          <div className='card subtle-shadow no-border'>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
-              <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-              <p className='card-text fs-2 '>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-            <div className='card-footer no-border'>
-              <small className='text-muted'>Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-        <div className='col-md-4'>
-          <div className='card subtle-shadow no-border'>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
-              <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-              <p className='card-text fs-2 '>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-            <div className='card-footer no-border'>
-              <small className='text-muted'>Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </>
-)
-//We can add this initial data to our props in our Portfolios component,
-//if none is defined at the _app.js component
+      </section>
+    </>
+  )
+}
+
+// getInitalProps for Portfolios Component
 Portfolios.getInitialProps = async () => {
   console.log('PORTFOLIOS getInitialProps')
-
-  const data = await apiCall()
-  return { ...data }
+  const portfolios = await fetchPortfolios()
+  return { portfolios } //return portfolios as properties to our page
 }
 
 export default Portfolios
--- server/index.js
+++ server/index.js
@@ -9,17 +9,77 @@ const dev = process.env.NODE_ENV !== 'production' //set a dev environment if not
 const app = next({ dev })
 const handle = app.getRequestHandler() // reqquest handler
 
+const data = {
+  portfolios: [
+    {
+      _id: 'sad87daasdhjdjsb',
+      title: 'Job in Netcentric',
+      company: 'Netcentric',
+      companyWebsite: 'www.google.com',
+      location: 'Spain, Barcelona',
+      jobTitle: 'Engineer',
+      description: 'Doing something, programing....',
+      startDate: '01/01/2014',
+      endDate: '01/01/2016'
+    },
+    {
+      _id: 'da789ad1',
+      title: 'Job in Siemens',
+      company: 'Siemens',
+      companyWebsite: 'www.google.com',
+      location: 'Slovakia, Kosice',
+      jobTitle: 'Software Engineer',
+      description: 'Responsoble for parsing framework for JSON medical data.',
+      startDate: '01/01/2011',
+      endDate: '01/01/2013'
+    },
+    {
+      _id: 'sadcxv9',
+      title: 'Work in USA',
+      company: 'WhoKnows',
+      companyWebsite: 'www.google.com',
+      location: 'USA, Montana',
+      jobTitle: 'Housekeeping',
+      description: 'So much responsibility....Overloaaaaaad',
+      startDate: '01/01/2010',
+      endDate: '01/01/2011'
+    }
+  ]
+}
+
 app.prepare().then(() => {
   const server = express() //create express server
+  // Construct a schema, using GRAPHQL schema language
   const schema = buildSchema(`
-    type Query {
-        hello: String 
-    }`)
+      type Porfolio {
+        _id: ID,
+        title: String,
+        company: String,
+        companyWebsite: String,
+        location: String,
+        jobTitle: String,
+        description: String,
+        startDate: String,
+        endDate: String
+      }
+      type Query {
+        hello: String
+        portfolio: Porfolio
+        portfolios: [Porfolio]
+      }
+  `)
+
   //name of query and return type
   // root provides a resolver for each API endpoint
   const root = {
     hello: () => {
       return 'Hello World'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
     }
   }
 
