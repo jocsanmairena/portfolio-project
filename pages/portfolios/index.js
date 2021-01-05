6ab56b334fa717f5dff28bd428d1c5442f18bdea
jocsanmairena <jocsan.mairena@gmail.com>
2021-01-04 18:32:36 -0600

    display PortfolioCard with fetch data on Portfolios Page

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { PortfolioCard } from '../../components/shared/portfolios/PortfolioCard'
 
 //making a axios request to retrive portfolios data using graphql
 const fetchPortfolios = () => {
@@ -32,58 +33,14 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
-        <button className='btn btn-primary' onClick={fetchPortfolios}>
-          Fetch Data
-        </button>
       </section>
-      {JSON.stringify(portfolios)}
       <section className='pb-5'>
         <div className='row'>
-          <div className='col-md-4'>
-            <div className='card subtle-shadow no-border'>
-              <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-                <p className='card-text fs-2'>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className='card-footer no-border'>
-                <small className='text-muted'>Last updated 3 mins ago</small>
-              </div>
+          {portfolios.map(portfolio => (
+            <div key={portfolio._id} className='col-md-4'>
+              <PortfolioCard portfolio={portfolio} />
             </div>
-          </div>
-          <div className='col-md-4'>
-            <div className='card subtle-shadow no-border'>
-              <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-                <p className='card-text fs-2 '>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className='card-footer no-border'>
-                <small className='text-muted'>Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className='col-md-4'>
-            <div className='card subtle-shadow no-border'>
-              <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-                <p className='card-text fs-2 '>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className='card-footer no-border'>
-                <small className='text-muted'>Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
