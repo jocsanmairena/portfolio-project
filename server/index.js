f6f267ca7ac9cdae08578053c7b51b190325e4d5
jocsanmairena <jocsan.mairena@gmail.com>
2021-02-25 03:43:07 -0500

    configured Apollo Server

--- server/index.js
+++ server/index.js
@@ -1,52 +1,54 @@
 //custom functionality for server side rendering
-const express = require("express")
-const next = require("next")
-const { graphqlHTTP } = require("express-graphql")
-const { buildSchema } = require("graphql")
+const express = require( "express" )
+const next = require( "next" )
 
-const port = parseInt(process.env.PORT, 10) || 3000 //set the port number. 3000 if local environment
+const { ApolloServer, gql } = require( 'apollo-server-express' )
+
+const port = parseInt( process.env.PORT, 10 ) || 3000 //set the port number. 3000 if local environment
 const dev = process.env.NODE_ENV !== "production" //set a dev environment if not equal to production
-const app = next({ dev })
+const app = next( { dev } )
 const handle = app.getRequestHandler() // reqquest handler
 
-const { portfolioResolvers } = require("./graphql/resolvers")
-const { portfolioTypes } = require("./graphql/types")
+const { portfolioQueries, portfolioMutations } = require( "./graphql/resolvers" )
+const { portfolioTypes } = require( "./graphql/types" )
 
-app.prepare().then(() => {
-	const server = express() //create express server
-	// STRUCTURE OF YOU DATA (SCHEMA): Construct a schema, using GRAPHQL schema language
+app.prepare().then( () => {
+  const server = express() //create express server
+  // STRUCTURE OF YOU DATA (SCHEMA): Construct a schema, using GRAPHQL schema language
 
-	//schema
-	const schema = buildSchema(`                         
+
+  const typeDefs = gql( `                         
     ${portfolioTypes}
       type Query {
         portfolio(id: ID): Portfolio
         portfolios: [Portfolio]
       }
+      
       type Mutation {
         createPortfolio( input: CreatePortfolioInput) : Portfolio
       }
   `)
 
-	// Revolvers
-	const resolvers = {
-		...portfolioResolvers,
-	}
-	server.use(
-		"/graphql",
-		graphqlHTTP({
-			schema,
-			rootValue: resolvers,
-			graphiql: true,
-			pretty: true,
-		}),
-	)
-	server.all("*", (req, res) => {
-		return handle(req, res)
-	})
+  // Revolvers
+  const resolvers = {
+    Query: {
+      ...portfolioQueries
+    },
+    Mutation: {
+      ...portfolioMutations
+    }
+  }
 
-	server.listen(port, (err) => {
-		if (err) throw err
-		console.log(`> Ready on http://localhost:${port}`)
-	})
-})
+  const apolloServer = new ApolloServer( { typeDefs, resolvers } )
+  apolloServer.applyMiddleware( { app: server } )
+
+
+  server.all( "*", ( req, res ) => {
+    return handle( req, res )
+  } )
+
+  server.listen( port, ( err ) => {
+    if ( err ) throw err
+    console.log( `> Ready on http://localhost:${port}` )
+  } )
+} )
